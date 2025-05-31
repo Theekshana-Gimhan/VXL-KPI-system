5edae25b17a9c693f59ca01c003e4fbc57d36ebe
Lenovo T480 <Lenovo T480@THEEK>
2025-05-31 22:26:50 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VXL_KPI_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
